cb2cd03f22be7778b57561cc8bb126c5d5e945c5
glenn-butler <i.glenn.azure@gmail.com>
2023-11-02 21:34:00 -0400

    Game board impl start

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((wasEditing) => !wasEditing);
+  }
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        value={playerName}
+        required
+        onChange={handlePlayerNameChange}
+      />
+    );
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
